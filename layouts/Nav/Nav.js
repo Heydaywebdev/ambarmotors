7587142f1a97decd186697bcf100504f7f557814
Heydaywebdev <167117611+Heydaywebdev@users.noreply.github.com>
2024-05-15 20:42:36 +0000

    Correccion en los metodos de prueba.

--- layouts/Nav/Nav.js
+++ layouts/Nav/Nav.js
@@ -5,82 +5,40 @@ export default {
   components: {},
   props: [],
   computed: {
-    ...mapGetters([
-      'getFavorites'
-    ])
+    ...mapGetters(['getFavorites'])
   },
   data() {
     return {
       showNav: false,
       languageActive: 'English',
       language: {
-        en: {
-          Name: 'English',
-          Key: 'US',
-          Flag: 'usa'
-        },
-        es: {
-          Name: 'Spain',
-          Key: 'ES',
-          Flag: 'spain'
-        }
+        en: { Name: 'English', Key: 'US', Flag: 'usa' },
+        es: { Name: 'Spain', Key: 'ES', Flag: 'spain' }
       },
-      shown: {
-        Language: false,
-        More: false
-      },
+      shown: { Language: false, More: false },
       items: [
-        {
-          title: 'vehicles',
-          to: 'https://express.ambarmotors.com/inventory/used'
-        },
-        {
-          title: 'sell-car',
-          to: { name: 'sell' }
-        },
-        {
-          title: 'financing',
-          to: { name: 'pre-approved' }
-        },
-        {
-          title: 'capitalOne',
-          to: { name: 'capitalOne' }
-        },
+        { title: 'vehicles', to: 'https://express.ambarmotors.com/inventory/used' },
+        { title: 'sell-car', to: { name: 'sell' } },
+        { title: 'financing', to: { name: 'pre-approved' } },
+        { title: 'capitalOne', to: { name: 'capitalOne' } },
       ],
       more: [
-        {
-          title: 'calculator',
-          to: { name: 'calculator' }
-        },
-        {
-          title: 'assurance',
-          to: { name: 'assurance' }
-        },
-        {
-          title: 'services',
-          to: { name: 'services' }
-        },
-        {
-          title: 'about',
-          to: { name: 'about' }
-        },
-        {
-          title: 'contact',
-          to: { name: 'contact' }
-        }
+        { title: 'calculator', to: { name: 'calculator' } },
+        { title: 'assurance', to: { name: 'assurance' } },
+        { title: 'services', to: { name: 'services' } },
+        { title: 'about', to: { name: 'about' } },
+        { title: 'contact', to: { name: 'contact' } }
       ],
       expanded: false
     }
   },
   methods: {
-
     ...mapActions(['toggleShowFavorites']),
-
     onCloseLang() {
-      this.shown.Language = false
+      this.shown.Language = false;
     },
     onCloseMore() {
-      this.shown.More = false
+      this.shown.More = false;
     },
     toggleExpand(menu) {
       for (const key of Object.keys(this.shown)) {
